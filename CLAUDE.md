73eea60e43abe79b735e7e74c9d885ac065545f4
Ezekiel Elin <ezekielelin@me.com>
2025-12-21 18:30:59 -0500

    Initial attempts and sample map

---
+++ CLAUDE.md
@@ -0,0 +1,9 @@
+The goal here is to re-implement the Minecraft Overviewer in Rust.
+
+The original source code is in Minecraft-Overviewer folder.
+
+The new code should go in src folder.
+
+Dependencies should be added with `cargo add ...`
+
+Ask the operator if you don't know how to accomplish a task.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::{
+    ffi::OsStr,
+    fs::{self},
+    io::{BufReader, Result},
+};
+
+use fastnbt::LongArray;
+use flate2::bufread::ZlibDecoder;
+use serde::Deserialize;
+
+fn main() -> Result<()> {
+    const SOURCE: &str = "sample_map/region";
+
+    let region_files = fs::read_dir(SOURCE)?.filter_map(|entry| {
+        let entry = entry.ok()?;
+        let path = entry.path();
+
+        if path.is_file() && path.extension() == Some(OsStr::new("mca")) {
+            return Some(entry);
+        } else {
+            return None;
+        }
+    });
+
+    for entry in region_files {
+        let file = fs::File::open(entry.path())?;
+        let reader = BufReader::new(file);
+
+        let mut decoder = ZlibDecoder::new(reader);
+        let mut data = vec![];
+        decoder.read_to_end(&mut data).unwrap();
+
+        let section: Section = fastnbt::from_bytes(&data).unwrap();
+
+        let states = section.block_states.unwrap();
+
+        for long in states.iter() {
+            println!("{}", long);
+        }
+    }
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Section {
+    pub block_states: Option<LongArray>,
+    pub y: i8,
 }
